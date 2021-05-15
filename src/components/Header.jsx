7ea697a9437d690a77a37958bda44f6a9ad9fd11
Krish Shah <63075612+krish-shahh@users.noreply.github.com>
2021-05-15 11:53:20 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div>
       <h1 className="text-center text-warning mt-3 mb-4"><a href="/">Coin Track</a></h1>
-      <h3 className="text-center text-warning mt-3 mb-4">By: <a href="https://krishshah.tech/" target="_blank">Krish Shah</a></h3>
+      <h4 className="text-center text-warning mt-3 mb-4">By: <a href="https://krishshah.tech/" target="_blank">Krish Shah</a></h4>
     </div>
   );
 };
