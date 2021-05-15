3babf87ae0900fad41a3ae825473b39286830a73
Krish Shah <63075612+krish-shahh@users.noreply.github.com>
2021-05-15 11:51:58 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <h1 className="text-center text-warning mt-3 mb-4"><a href="/">Coin Track</a>by: Krish Shah</h1>
+      <h1 className="text-center text-warning mt-3 mb-4"><a href="/">Coin Track</a></h1>
+      <h3 className="text-center text-warning mt-3 mb-4">By: <a href="https://krishshah.tech/" target="_blank">Krish Shah</a></h3>
     </div>
   );
 };
